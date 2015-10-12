1526a6a44e9099664b144c1cf0ca4819734a6b50
Markov Alex <markovalex95@gmail.com>
2015-10-12 18:41:21 +0300

    Начальный коммит

---
+++ LoggerException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogger
+{
+	/// <summary>
+	/// Исключение, если логгер уже настроен
+	/// </summary>
+	class LoggerAlreadySetupException : Exception
+	{
+		public LoggerAlreadySetupException(): base("Логгер уже создан или настроен")
+		{
+			
+		}
+
+		public LoggerAlreadySetupException(string message) : base(message)
+		{
+
+		}
+	}
+}
